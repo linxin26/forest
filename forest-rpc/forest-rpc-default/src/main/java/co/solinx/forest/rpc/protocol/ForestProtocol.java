580af0cd10f89d6fb65bfbe14a4db88f19b9399b
linxin26 <644299146@qq.com>
2015-04-06 21:37:08 +0800

    重构JDK动态代理。

--- forest-rpc/forest-rpc-default/src/main/java/co/solinx/forest/rpc/protocol/ForestProtocol.java
+++ forest-rpc/forest-rpc-default/src/main/java/co/solinx/forest/rpc/protocol/ForestProtocol.java
@@ -1,7 +1,5 @@
 package co.solinx.forest.rpc.protocol;
 
-import co.solinx.forest.config.RegistryConfig;
-import co.solinx.forest.registry.zookeeper.ZookeeperRegistry;
 import org.apache.log4j.Logger;
 
 /**
@@ -11,9 +9,9 @@ public class ForestProtocol {
 
     Logger logger=Logger.getLogger(ForestProtocol.class);
 
-    ZookeeperRegistry zookeeperRegistry = ZookeeperRegistry.getZookeeper();
+//    ZookeeperRegistry zookeeperRegistry = ZookeeperRegistry.getZookeeper();
 
-    public void export(String interfaceName,RegistryConfig registryAddress) {
+//    public void export(String interfaceName,RegistryConfig registryAddress) {
         //forest://192.168.254.144:20909/co.solinx.forest.demo.impl.HelloForestServiceImpl
 //        String forestProtocol="forest://";
 //
@@ -35,6 +33,6 @@ public class ForestProtocol {
 //            this.startServer();
 //        }
 
-    }
+//    }
 
 }
---
+++ forest-rpc/forest-rpc-default/src/main/java/co/solinx/forest/rpc/protocol/handler/JdkProxyHandler.java
@@ -0,0 +1,30 @@
+package co.solinx.forest.rpc.protocol.handler;
+
+import co.solinx.forest.remote.invoker.NettyInvoker;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by LX on 2015/4/4.
+ */
+public class JdkProxyHandler implements InvocationHandler{
+
+
+    private Object delegate;
+    private String serverAddress;
+
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        NettyInvoker invoker=new NettyInvoker();
+
+        return invoker.clientInvoker(delegate,serverAddress,method);
+    }
+
+    public JdkProxyHandler(Object delegate, String serverAddress) {
+        this.delegate = delegate;
+        this.serverAddress = serverAddress;
+    }
+}
