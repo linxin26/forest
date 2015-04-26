1b1b06b3f49976126428bb6cd224bc2ae27d8322
linxin26 <644299146@qq.com>
2015-04-26 21:17:35 +0800

    add forest-rpc-default

---
+++ forest-rpc/forest-rpc-default/src/main/java/co/solinx/forest/rpc/ForestInvoker.java
@@ -0,0 +1,12 @@
+package co.solinx.forest.rpc;
+
+import cn.solinx.forest.rpc.api.AbstractInvoker;
+
+/**
+ * Created by linx on 2015/4/26.
+ */
+public class ForestInvoker extends AbstractInvoker {
+
+
+
+}
---
+++ forest-rpc/forest-rpc-default/src/test/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by linx on 2015/4/26.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
